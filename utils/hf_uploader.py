e5c6d4e599022bba4927ee42e8098009fa1d3523
NevulaDP <nevobetesh@gmail.com>
2025-05-20 10:07:08 +0300

    Update hf_uploader.py

--- utils/hf_uploader.py
+++ utils/hf_uploader.py
@@ -1,18 +1,15 @@
+import os
 import json
+from huggingface_hub import upload_file
+from utils.hf_config import HF_REPO_ID, HF_TOKEN
 
 def upload_image_to_hub(image_file, property_uuid, save_dir="temp_images"):
-    import os
-    from huggingface_hub import upload_file
-    from utils.hf_config import HF_REPO_ID, HF_TOKEN
-
-    # Save to a temporary local file
     os.makedirs(save_dir, exist_ok=True)
     file_path = os.path.join(save_dir, image_file.name)
 
     with open(file_path, "wb") as f:
         f.write(image_file.getbuffer())
 
-    # Upload to HF with UUID in path
     hf_path = f"images/{property_uuid}/{image_file.name}"
     upload_file(
         path_or_fileobj=file_path,
@@ -21,7 +18,6 @@ def upload_image_to_hub(image_file, property_uuid, save_dir="temp_images"):
         repo_type="dataset",
         token=HF_TOKEN
     )
-
     return f"https://huggingface.co/datasets/{HF_REPO_ID}/resolve/main/{hf_path}"
 
 def upload_json_to_hub(entries, filename="property_db.json"):
