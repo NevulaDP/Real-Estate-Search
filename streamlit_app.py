8fecc9b5a9c1536d37c9684766241a01c4338973
NevulaDP <nevobetesh@gmail.com>
2025-05-20 10:33:37 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ if "form_inputs" not in st.session_state:
 
 # --- SIDEBAR ---#
 st.sidebar.title("🧠 Smarter Property Engine")
-st.sidebar.markdown("## 🔍 Navigation")
+st.sidebar.markdown("### 🔍 Navigation")
 mode = st.sidebar.radio("Go to:",["🏡 Upload Property", "🔎 Search Properties"])
 
 if mode == "🏡 Upload Property":
