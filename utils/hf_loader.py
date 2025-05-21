8c0376dd899fee7219e95a366eed1f297ac047da
NevulaDP <nevobetesh@gmail.com>
2025-05-21 15:45:48 +0300

    Update hf_loader.py

--- utils/hf_loader.py
+++ utils/hf_loader.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from utils.hf_config import HF_REPO_ID
+import streamlit as st
 
 def load_entries_from_hub(filename="property_db.json"):
     url = f"https://huggingface.co/datasets/{HF_REPO_ID}/resolve/main/{filename}"
